7f84f3b343a38dd313376a4af92208ce05613657
Chris Rees <crees@FreeBSD.org>
2023-11-04 11:48:14 +0000

    Add nsupdate script

---
+++ templates/root/cronjobs/nsupdate-script.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Need to include rndc.conf with the correct key
+
+ip=$(/usr/local/bin/dig +short myip.opendns.com @resolver1.opendns.com)
+
+#update delete elgar.ourspaceplaytherapy.co.uk. A
+#update add elgar.ourspaceplaytherapy.co.uk. 600 A $ip
+/usr/local/bin/nsupdate -k /usr/local/etc/namedb/rndc.conf << EOF
+server chrysaor.bayofrum.net
+zone ourspaceplaytherapy.co.uk
+update delete ourspaceplaytherapy.co.uk. A
+update add ourspaceplaytherapy.co.uk. 600 A $ip
+send
+EOF
